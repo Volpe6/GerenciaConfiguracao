d70422e6a3e14f29cd738030d9a243182fc5d73d
Volpe6 <baasch2014@gmail.com>
2020-08-31 21:36:07 -0300

    Ajusta as rotas

--- src/rotas.js
+++ src/rotas.js
@@ -4,14 +4,14 @@ const controllerProduto = require('./controller/controllerProduto');
 const rotas = express.Router();
 
 //Rotas de produto
-rotas.get('/produto'            , controllerProduto.index);//retorna todos os produtos
-rotas.get('/produto/:id'        , controllerProduto.findById);//retorna um produto de id especifico
+rotas.get('/produto'           , controllerProduto.index);//retorna todos os produtos
+rotas.get('/produto/:id'       , controllerProduto.findById);//retorna um produto de id especifico
+rotas.get('/produto/remove/:id', controllerProduto.remove);//remove um produto pelo id
 /*
 atualiza/cria um registro
 para atualizar um registro basta passar o id junto das outras informações no json,
 para criar um registro basta passar um id em branco nas requisições
 */
-rotas.post('/produto'           , controllerProduto.store);
-rotas.post('/produto/remove/:id', controllerProduto.remove);
+rotas.post('/produto', controllerProduto.store);
 
 module.exports = rotas;

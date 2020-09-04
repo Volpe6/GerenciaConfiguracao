c5d509d71450c0fd9a98b696800db122378f1574
jefip <jefipnz@gmail.com>
2020-09-03 21:31:34 -0300

    Criação do formulário de cadastro.

--- public/base.js
+++ public/base.js
@@ -10,19 +10,36 @@ const ControlePagina = (function(){
     }
 
     function carregaHtml(sLink){
-        oContainer.empty();
+        oContainer.hide();
         $.get('/pages/' + sLink).then(function(sHtml){
             oContainer.html(sHtml);
+            oContainer.show();
+            window.history.pushState({ "html": sHtml }, "", '?p=' + sLink.replace(/.html$/, ''));
+        }, function(){
+            carregaHtml('bemVindo.html');
         });
     }
 
-    $(window).on('load', function(){
+    $(window).on('popstate', function(e){
+        oContainer.empty();
+        e = e.originalEvent;
+        if(e.state){
+            oContainer.html(e.state.html);
+        }
+    }).on('load', function(){
         oContainer = $('#base_pagina');
         $.get('//cdn.datatables.net/plug-ins/1.10.21/i18n/Portuguese-Brasil.json').then(function(oRes){
             ControlePagina.DATA_TABLES_L18N = oRes;
         })
         iniciaNavBar();
-        carregaHtml('bemVindo.html')
+        var params = new URLSearchParams(document.location.search.substring(1));
+        var page   = params.get("p");
+        if(page){
+            carregaHtml(page + '.html')
+        }
+        else {
+            carregaHtml('bemVindo.html')
+        }
     });
     return {
          carregaPagina: carregaHtml
